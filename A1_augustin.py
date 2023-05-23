7a5907bbd188ffa0890f162f2a9be221edbb973a
Hannah <hannah.augustin@gmail.com>
2023-05-23 11:45:19 +0200

    Create A1_augustin.py

---
+++ A1_augustin.py
@@ -0,0 +1,28 @@
+"""A1
+Merge this pull request for full points. :)
+"""
+#
+#
+### Quote:
+#
+# "Perhaps the emotion of time is precisely what time is for us." - Carlo Rovelli
+#
+#
+### Disliked food/flavour:
+#
+# I cannot stand the crunch of raw celery.
+#
+### Recommenation:
+#
+# "Ways of Being: Animals, Plants, Machines: The Search for a Planetary Intelligence " by James Bridle
+#
+'''Another quote from the recommended book:
+“We can’t read water in the same way as we can’t read data…Working with it makes us more aware of the distance between ourselves and the matter under consideration: it reminds us that we share this world rather than own it. Knowledge produced through the medium of the shifting surface of a bucket of water is made in cooperation with the world, rather than by conquering it.”
+― James Bridle, Ways of Being: Animals, Plants, Machines: The Search for a Planetary Intelligence 
+'''
+#
+#
+### German word:
+#
+# "Mahlzeit" Austrian for "enjoy your meal", but also used as a greeting to say hello to anyone around lunchtime (11-14:00)
+#
